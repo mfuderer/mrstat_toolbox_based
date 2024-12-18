11b721d4b752bc8b0bcfb04a7fea6b213c4ac655
Miha Fuderer <m.fuderer@umcutrecht.nl>
2024-12-18 10:32:27 +0100

    RF_shapes debug

--- numerical_phantom/RF_Shapes.jl
+++ numerical_phantom/RF_Shapes.jl
@@ -53,6 +53,7 @@ function RF_from_file(nTR, ny)
     #@assert(nTR==5*ny) # this function is only meaningful for a 5-measurement "actual" setup
     fn = recon_options["rfFile"]
     fPath = string(recon_options["rfFolder"],fn,".jld2")
+    @show fPath
     vars = FileIO.load(fPath)
     RFdeg = vars["RFdeg"]
     RF_train = vec(complex.(RFdeg))
